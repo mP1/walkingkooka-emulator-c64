25f24ca3277ab83e5ecaa66c1c25c0079feda1c4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-12-29 16:35:52 +1100

    CpuInstructionsTest.testInstructionsUniqueOpCodes()

--- src/test/java/walkingkooka/emulator/c64/CpuInstructionsTest.java
+++ src/test/java/walkingkooka/emulator/c64/CpuInstructionsTest.java
@@ -17,12 +17,66 @@
 
 package walkingkooka.emulator.c64;
 
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.map.Maps;
+import walkingkooka.collect.set.Sets;
 import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.reflect.MethodAttributes;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 
 import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public final class CpuInstructionsTest implements PublicStaticHelperTesting<CpuInstructions> {
+
+    @Test
+    public void testInstructionsUniqueOpCodes() throws Exception {
+        final Map<Byte, Set<CpuInstruction>> opcodeToCpuInstruction = Maps.sorted();
+
+        int i = 0;
+
+        for (final Method method : CpuInstructions.class.getDeclaredMethods()) {
+            if (MethodAttributes.STATIC.is(method) && JavaVisibility.of(method) == JavaVisibility.PUBLIC) {
+                if (method.getReturnType() == CpuInstruction.class && method.getParameterCount() == 0) {
+                    final CpuInstruction cpuInstruction = (CpuInstruction) method.invoke(null);
+
+                    final Byte opcode = cpuInstruction.opcode();
+
+                    Set<CpuInstruction> same = opcodeToCpuInstruction.get(opcode);
+                    if (null == same) {
+                        same = Sets.hash();
+                        opcodeToCpuInstruction.put(opcode, same);
+                    }
+
+                    same.add(cpuInstruction);
+
+                    i++;
+                }
+            }
+        }
+
+        for (final Iterator<Set<CpuInstruction>> sames = opcodeToCpuInstruction.values().iterator(); sames.hasNext(); ) {
+            final Set<CpuInstruction> same1 = sames.next();
+            if (same1.size() == 1) {
+                sames.remove();
+            }
+        }
+
+        this.checkEquals(
+            Maps.empty(),
+            opcodeToCpuInstruction
+        );
+
+        this.checkNotEquals(
+            0,
+            i
+        );
+    }
+
+    // class............................................................................................................
+
     @Override
     public boolean canHavePublicTypes(final Method method) {
         return false;
