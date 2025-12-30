ce5a85809963d3426aaf5c36b6bcf0aad4c97826
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-12-30 13:45:28 +1100

    CpuInstructionTransferPha

---
+++ src/test/java/walkingkooka/emulator/c64/CpuInstructionTransferPhaTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.emulator.c64;
+
+import org.junit.jupiter.api.Test;
+
+public final class CpuInstructionTransferPhaTest extends CpuInstructionTransferTestCase<CpuInstructionTransferPha> {
+
+    @Test
+    public void testExecute() {
+        final short pc = 0x1000;
+
+        final CpuContext context = CpuContexts.basic(
+            AddressBuses.memory(
+                256 * 256
+            )
+        );
+        context.setPc(pc);
+
+        final byte a = 0x55;
+        context.setA(a);
+
+        context.setStackPointer((byte) 0x40);
+
+        context.setFlags((byte) 0);
+
+        this.executeAndCheck(
+            this.createCpuInstruction(),
+            context,
+            a,
+            context.x(),
+            context.y(),
+            "-----1--",
+            (byte) 0x3f, // stackPointer
+            pc
+        );
+
+        this.readByteAndCheck(
+            context,
+            (short) 0x140,
+            a
+        );
+    }
+
+    @Override
+    public CpuInstructionTransferPha createCpuInstruction() {
+        return CpuInstructionTransferPha.INSTANCE;
+    }
+
+    @Override
+    public Class<CpuInstructionTransferPha> type() {
+        return CpuInstructionTransferPha.class;
+    }
+}
