4f3e0ba6b9cd42c4daadf24e2022985fa5178cb9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-01-01 15:56:30 +1100

    CpuInstructionSharedOperandRegisterA

---
+++ src/main/java/walkingkooka/emulator/c64/CpuInstructionSharedOperandRegisterA.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.emulator.c64;
+
+final class CpuInstructionSharedOperandRegisterA extends CpuInstructionSharedOperandRegister {
+
+    final static CpuInstructionSharedOperandRegisterA INSTANCE = new CpuInstructionSharedOperandRegisterA();
+
+    private CpuInstructionSharedOperandRegisterA() {
+        super();
+    }
+
+    @Override
+    byte readRegister(final CpuContext context) {
+        return context.a();
+    }
+
+    @Override
+    void writeRegister(final byte value,
+                       final CpuContext context) {
+        context.setA(value);
+        CpuInstructionShared.setMinusAndZero(
+            value,
+            context
+        );
+    }
+}
