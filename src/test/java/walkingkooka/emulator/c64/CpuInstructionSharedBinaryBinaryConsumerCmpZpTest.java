a735cd6dfbb15874c9902ff0ae5b972950e1ffdf
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-01-04 19:35:03 +1100

    CpuInstructionSharedBinaryBinaryConsumerCmpZp

---
+++ src/test/java/walkingkooka/emulator/c64/CpuInstructionSharedBinaryBinaryConsumerCmpZpTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.emulator.c64;
+
+import org.junit.jupiter.api.Test;
+
+public final class CpuInstructionSharedBinaryBinaryConsumerCmpZpTest extends CpuInstructionSharedBinaryBinaryConsumerCmpTestCase<CpuInstructionSharedBinaryBinaryConsumerCmpZp> {
+
+    @Test
+    public void testExecuteGreater() {
+        final CpuContext context = CpuContexts.basic(
+            AddressBuses.memory(256 * 256)
+        );
+
+        final short pc = 0x1000;
+        context.setPc(pc);
+
+        final byte offset = 0x40;
+
+        context.writeAddress(
+            pc,
+            offset
+        );
+
+        context.writeZeroPageByte(
+            offset,
+            (byte) 0xc0
+        );
+
+        context.setA(
+            (byte) 0xff
+        );
+
+        this.executeAndCheck(
+            CpuInstructionSharedBinaryBinaryConsumerCmpZp.INSTANCE,
+            context,
+            "C----1--"
+        );
+    }
+
+    @Test
+    public void testExecuteEquals() {
+        final CpuContext context = CpuContexts.basic(
+            AddressBuses.memory(256 * 256)
+        );
+
+        final short pc = 0x1000;
+        context.setPc(pc);
+
+        final byte offset = 0x40;
+
+        context.writeAddress(
+            pc,
+            offset
+        );
+
+        context.writeZeroPageByte(
+            offset,
+            (byte) 0xc0
+        );
+
+        context.setA(
+            (byte) 0xc0
+        );
+
+        this.executeAndCheck(
+            CpuInstructionSharedBinaryBinaryConsumerCmpZp.INSTANCE,
+            context,
+            "CZ---1--"
+        );
+    }
+
+    @Test
+    public void testExecuteLess() {
+        final CpuContext context = CpuContexts.basic(
+            AddressBuses.memory(256 * 256)
+        );
+
+        final short pc = 0x1000;
+        context.setPc(pc);
+
+        final byte offset = 0x40;
+
+        context.writeAddress(
+            pc,
+            offset
+        );
+
+        context.writeZeroPageByte(
+            offset,
+            (byte) 0xc0
+        );
+
+        context.setA(
+            (byte) 0x1
+        );
+
+        this.executeAndCheck(
+            CpuInstructionSharedBinaryBinaryConsumerCmpZp.INSTANCE,
+            context,
+            "-----1--"
+        );
+    }
+
+    @Override
+    public CpuInstructionSharedBinaryBinaryConsumerCmpZp createCpuInstruction() {
+        return CpuInstructionSharedBinaryBinaryConsumerCmpZp.INSTANCE;
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<CpuInstructionSharedBinaryBinaryConsumerCmpZp> type() {
+        return CpuInstructionSharedBinaryBinaryConsumerCmpZp.class;
+    }
+}
